74540cd34c61950ce40595af52718ba647b25b8e
omtiwari52 <tiwariom900@gmail.com>
2024-02-24 02:35:03 +0530

    Project Done

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to(".imagecontainer",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2, 
+    stagger: 2,
+})
+gsap.to(".text h1" ,{
+    delay : 1,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: 0
+})
+gsap.to(".text h1" ,{
+    delay : 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%"
+})
